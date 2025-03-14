6a083703cb30b476cb1bbcb1b708ffdef745bf2d
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2025-03-14 10:01:00 +0100

    Optimisation du SEO et Correctifs Finaux

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
-const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase("https://festivhalloween.nicolas-thai.eu:443");
 
 export { pb };
 
